ca901d20ac96f631a3931a272829bd3d9194591e
nathanael-lin <nathanael.lin@gmail.com>
2026-08-18 16:09:24 +0800

    Update dealership.js

--- server/database/dealership.js
+++ server/database/dealership.js
@@ -1,43 +1,44 @@
-const mongoose = require('mongoose');
+/* jshint esversion: 6, node: true */
 
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const dealerships = new Schema({
-	id: {
-    type: Number,
-    required: true,
-	},
-	city: {
-    type: String,
-    required: true
-  },
-  state: {
-    type: String,
-    required: true
-  },
-  address: {
-    type: String,
-    required: true
-  },
-  zip: {
-    type: String,
-    required: true
-  },
-  lat: {
-    type: String,
-    required: true
-  },
-  long: {
-    type: String,
-    required: true
-  },
-  short_name: {
-    type: String,
-  },
-  full_name: {
-    type: String,
-    required: true
-  }
+    id: {
+        type: Number,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    zip: {
+        type: String,
+        required: true
+    },
+    lat: {
+        type: String,
+        required: true
+    },
+    long: {
+        type: String,
+        required: true
+    },
+    short_name: {
+        type: String
+    },
+    full_name: {
+        type: String,
+        required: true
+    }
 });
 
-module.exports = mongoose.model('dealerships', dealerships);
+module.exports = mongoose.model("dealerships", dealerships);
