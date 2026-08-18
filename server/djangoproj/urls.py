3de14e345d3d6f4e7b2eb5ff20a871e632879fc0
nathanael-lin <nathanael.lin@gmail.com>
2026-08-18 16:00:14 +0800

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,22 +1,23 @@
-"""djangoproj URL Configuration
+"""djangoproj URL Configuration.
 
 The `urlpatterns` list routes URLs to views.
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-from django.conf.urls.static import static
+
 from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path("admin/", admin.site.urls),
+    path("djangoapp/", include("djangoapp.urls")),
+    path("", TemplateView.as_view(template_name="Home.html")),
+    path("about/", TemplateView.as_view(template_name="About.html")),
+    path("contact/", TemplateView.as_view(template_name="Contact.html")),
+    path("login/", TemplateView.as_view(template_name="index.html")),
+    path("register/", TemplateView.as_view(template_name="index.html")),
+    path("dealers/", TemplateView.as_view(template_name="index.html")),
+    path("dealer/", TemplateView.as_view(template_name="index.html")),
+    path("postreview/", TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
