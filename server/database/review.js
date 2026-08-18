629775c49828609904e6ed30cbe58019790af87b
nathanael-lin <nathanael.lin@gmail.com>
2026-08-18 16:09:42 +0800

    Update review.js

--- server/database/review.js
+++ server/database/review.js
@@ -1,44 +1,45 @@
-const mongoose = require('mongoose');
+/* jshint esversion: 6, node: true */
 
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const reviews = new Schema({
-	id: {
-    type: Number,
-    required: true,
-	},
-	name: {
-    type: String,
-    required: true
-  },
-  dealership: {
-    type: Number,
-    required: true,
-  },
-  review: {
-    type: String,
-    required: true
-  },
-  purchase: {
-    type: Boolean,
-    required: true
-  },
-  purchase_date: {
-    type: String,
-    required: true
-  },
-  car_make: {
-    type: String,
-    required: true
-  },
-  car_model: {
-    type: String,
-    required: true
-  },
-  car_year: {
-    type: Number,
-    required: true
-  },
+    id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    dealership: {
+        type: Number,
+        required: true
+    },
+    review: {
+        type: String,
+        required: true
+    },
+    purchase: {
+        type: Boolean,
+        required: true
+    },
+    purchase_date: {
+        type: String,
+        required: true
+    },
+    car_make: {
+        type: String,
+        required: true
+    },
+    car_model: {
+        type: String,
+        required: true
+    },
+    car_year: {
+        type: Number,
+        required: true
+    }
 });
 
-module.exports = mongoose.model('reviews', reviews);
+module.exports = mongoose.model("reviews", reviews);
